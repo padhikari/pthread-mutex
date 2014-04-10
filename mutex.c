9520469d5384f32f19d2a92a033d3cf778fba6d5
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-04-10 13:03:56 -0700

    mutex.c

--- mutex.c
+++ mutex.c
@@ -8,6 +8,16 @@ int counter = 0;
 
 int main(){
     
+    pthread_t thread1,thread2;
+    
+    //create thread1 and thread2
+    pthread_create(&thread1,NULL,&countFunction,NULL);
+    pthread_create(&thread2,NULL,&countFunction,NULL);
+    
+    //wait until both thread will finish their routines
+    pthread_join(thread1,NULL);
+    pthread_join(thread2,NULL);
+    
     exit(0);
 }
 
